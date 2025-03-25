19e6bb84b6a7acdd6b8996ac3176d4ded16f8846
Thatoneperon <Thatoneperon1@gmail.com>
2025-03-24 22:00:10 -0700

    FINAL CHANGES

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./style.css"
 export default function App() {
   return (
     <>
-      <h1>Welcome to your Task Manager</h1>
-      <p>You will come here to check your teams tasks as well as your own. <br/>
+      <h1 style={{textAlign: 'center'}}>Welcome to your Task Manager</h1>
+      <p style={{textAlign: 'center'}}>You will come here to check your teams tasks as well as your own. <br/>
       You will be able to move your task to the completed tab once you<br/> have accomplished
       them so an admin can review your work. 
       </p>
-      <h7>Welcome!</h7>
+      <h6 style={{textAlign: 'center'}}>Welcome!</h6>
     </>
   );
 }
