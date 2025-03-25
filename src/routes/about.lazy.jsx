5dcb9cfbf22d270a58dddc26e78e6355cfdabfc6
matthew-wehrmeyer <mwehrmeyer@csub.edu>
2025-03-24 21:29:50 -0700

    Added more on about page

--- src/routes/about.lazy.jsx
+++ src/routes/about.lazy.jsx
@@ -7,11 +7,16 @@ export const Route = createLazyFileRoute("/about")({
 function About() {
   return (
     <div className="about-page">
-      <h1>ABOUT</h1>
-      <h3>Goal</h3>
-      <p>
+      <h1 style={{textAlign: 'center'}}>ABOUT</h1>
+      <h3 style={{textAlign: 'center'}}>Goal</h3>
+      <p style={{textAlign: 'center'}}>
         Goal of this app is to be able manage tasks with groups of people.
       </p>
+      <p style={{textAlign: 'center'}}>
+        To be able to indicate tasks that are assigned to certain users. 
+        Whether it is managing your own tasks; based on what is not started, in process, or completed. As well as veiweing others tasks
+      </p>
+      <p style={{textAlign: 'center'}}>This includes being able to assign tasks to users, with setting dates, and priority of the task</p>
     </div>
   );
 }
