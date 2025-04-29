179f34a4d4d8a2803534520416043d232ec454b6
krrithikezhil30 <krrithikezhil30@gmail.com>
2025-04-29 17:48:06 +0000

    error fix

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Login from '../components/Login';
+import { Route } from '@tanstack/react-router';
+import { rootRoute } from './__root';
+
+export const LoginRoute = new Route({ getParentRoute: () => rootRoute, path: '/login' });
 
 const LoginPage = () => {
   return (
